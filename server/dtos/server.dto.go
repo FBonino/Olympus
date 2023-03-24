2da7c2882936ad21e0b367edcbb8a526f22be53b
FBonino <boninoffranco@gmail.com>
2023-03-24 15:16:51 -0300

    Fix: Add missing role information to DTO

--- server/dtos/server.dto.go
+++ server/dtos/server.dto.go
@@ -1,6 +1,19 @@
 package dtos
 
-import "server/models"
+import (
+	"fmt"
+	"server/models"
+)
+
+type ServerUserDTO struct {
+	ID           string              `json:"id" bson:"_id"`
+	Username     string              `json:"username" bson:"username"`
+	Email        string              `json:"email" bson:"email"`
+	Avatar       string              `json:"avatar" bson:"avatar"`
+	Status       string              `json:"status" bson:"status"`
+	CustomStatus string              `json:"customStatus" bson:"customStatus"`
+	Roles        []models.ServerRole `json:"roles" bson:"roles"`
+}
 
 type ChannelDTO struct {
 	ID   string `json:"id" bson:"_id"`
@@ -17,12 +30,12 @@ type ServerDTO struct {
 }
 
 type ServerExtendedDTO struct {
-	ID             string       `json:"id" bson:"_id"`
-	Name           string       `json:"name" bson:"name"`
-	Avatar         string       `json:"avatar" bson:"avatar"`
-	Channels       []ChannelDTO `json:"channels" bson:"channels"`
-	DefaultChannel string       `json:"defaultChannel" bson:"defaultChannel"`
-	Users          []UserDTO    `json:"users" bson:"users"`
+	ID             string          `json:"id" bson:"_id"`
+	Name           string          `json:"name" bson:"name"`
+	Avatar         string          `json:"avatar" bson:"avatar"`
+	Channels       []ChannelDTO    `json:"channels" bson:"channels"`
+	DefaultChannel string          `json:"defaultChannel" bson:"defaultChannel"`
+	Users          []ServerUserDTO `json:"users" bson:"users"`
 }
 
 func MapChannelDTO(channel models.Channel) ChannelDTO {
@@ -66,12 +79,31 @@ func MapServersDTO(servers []*models.Server) []ServerDTO {
 }
 
 func MapServerExtendedDTO(server *models.Server, users []*models.User) ServerExtendedDTO {
+	var serverUsersDTO []ServerUserDTO
+
+	usersDTO := MapUsersDTO(users)
+
+	for i, user := range server.Users {
+		fmt.Println(i, user, usersDTO[i])
+		var serverUser ServerUserDTO = ServerUserDTO{
+			ID:           usersDTO[i].ID,
+			Username:     usersDTO[i].Username,
+			Email:        usersDTO[i].Email,
+			Avatar:       usersDTO[i].Avatar,
+			Status:       usersDTO[i].Status,
+			CustomStatus: usersDTO[i].CustomStatus,
+			Roles:        user.Roles,
+		}
+
+		serverUsersDTO = append(serverUsersDTO, serverUser)
+	}
+
 	return ServerExtendedDTO{
 		ID:             server.ID,
 		Name:           server.Name,
 		Avatar:         server.Avatar,
 		Channels:       MapChannelsDTO(server.Channels),
 		DefaultChannel: server.DefaultChannel,
-		Users:          MapUsersDTO(users),
+		Users:          serverUsersDTO,
 	}
 }
