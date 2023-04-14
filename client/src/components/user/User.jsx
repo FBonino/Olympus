b5911ef08045d5e5f27068ce2a1a38496da7d1d5
FBonino <boninoffranco@gmail.com>
2023-04-14 15:32:23 -0300

    Style: Add active style to currently open dm

--- client/src/components/user/User.jsx
+++ client/src/components/user/User.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import style from "./User.module.css";
 import mapStatusColor from "../../helpers/mapStatusColor";
 
-const User = ({ username, status, customStatus, avatar, color, nav }) => {
+const User = ({ username, status, customStatus, avatar, color, nav, active }) => {
   const contextMenu = !nav ? "avatar" : undefined
 
   return (
-    <div contextMenu={contextMenu} className={style.container}>
+    <div contextMenu={contextMenu} className={style.container} id={active ? style.active : ""}>
       <div contextMenu={contextMenu} className={style.image}>
         <img contextMenu={contextMenu} className={nav ? style.avatarNav : style.avatar} src={`${process.env.REACT_APP_API}/uploads/${avatar}`} alt="" />
-        <div className={nav ? style.statusNav : style.status} style={{ backgroundColor: mapStatusColor(status) }} />
+        {
+          !!status && <div className={nav ? style.statusNav : style.status} style={{ backgroundColor: mapStatusColor(status) }} />
+        }
       </div>
       <div contextMenu={contextMenu} className={style.text}>
         <span contextMenu={contextMenu} className={nav ? style.usernameNav : style.username} style={{ color }}> {username} </span>
